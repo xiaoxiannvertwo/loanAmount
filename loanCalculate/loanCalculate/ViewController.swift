f61f7570ee9f0311087927791c8e8469adda2313
上海海洋大学 <shanhaikaiyoudaigaku@shanghaihaiyangdaxuedeimac-3.local>
2017-03-23 15:48:01 +0800

    第二周学习

--- loanCalculate/loanCalculate/ViewController.swift
+++ loanCalculate/loanCalculate/ViewController.swift
@@ -9,7 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+    @IBOutlet weak var loanAmountField: UITextField!
+    @IBOutlet weak var yearField: UITextField!
+    @IBOutlet weak var interestRateField: UITextField!
+    @IBOutlet weak var resultField: UILabel!
+    
+    @IBAction func calculateField(_ sender: Any) {
+    let calculate1 = simpleCalculate()
+        
+        resultField.text = calculate1.simplecalculate(LoanAmount: Double(loanAmountField.text!)!, years: Int (yearField.text!)!, interestRate: Double(interestRateField.text!)!).description
+    }
 
+    @IBAction func compoundcalculateField(_ sender: Any) {
+        let calculate2 = compoundCalculate()
+        
+        resultField.text = calculate2.calculate(LoanAmount: Double(loanAmountField.text!)!, years: Int (yearField.text!)!, interestRate: Double(interestRateField.text!)!).description
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +37,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+    
+    
+class simpleCalculate {
+    func simplecalculate(LoanAmount:Double,years:Int,interestRate:Double)->Double{
+        let rate=interestRate/100
+        let interest=LoanAmount*rate*Double (years)
+        return LoanAmount+interest
+    }
+    
+}
+  class compoundCalculate {
+    func calculate(LoanAmount:Double,years:Int,interestRate:Double) -> Double{
+        let  rate=interestRate/100
+        let compoundInterest=pow(1.0+rate, Double(years))
+        return compoundInterest*LoanAmount
+    }
+}
+
 
